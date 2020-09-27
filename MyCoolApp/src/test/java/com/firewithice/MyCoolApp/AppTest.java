f48bafaec3487b3e76047c4283fa2e0e52839b87
Jenish16 <22037861+Jenish16@users.noreply.github.com>
2020-09-27 18:15:41 +0530

    Basic Maven Project Added

---
+++ MyCoolApp/src/test/java/com/firewithice/MyCoolApp/AppTest.java
@@ -0,0 +1,20 @@
+package com.firewithice.MyCoolApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
